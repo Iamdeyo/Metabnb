d7e9dbc86c9d0b4579ebcd30d0e6f0aa1bc668e1
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-11-16 09:37:23 +0100

    style: footer style fixed

--- src/compontents/Footer.jsx
+++ src/compontents/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
   return (
     <footer>
       <div className="bg-[#1D1D1E]">
-        <div className="container p-4 pt-10 pb-8 mx-auto text-[#F1F3F9]">
+        <div className="container p-4 sm:px-0 pt-10 pb-8 mx-auto text-[#F1F3F9]">
           <div className="flex flex-wrap justify-between ">
-            <div className="w-1/2 mt-8 lg:w-1/4 md:w-1/3 md:mt-0">
+            <div className="w-1/2 mt-8 xxl:w-1/4 xl:w-1/3 xl:mt-0">
               <p className="text-white font-bold text-lg">Community</p>
 
               <ul className="flex flex-col items-start gap-2 mt-5">
@@ -26,7 +26,7 @@ function Footer() {
                 </li>
               </ul>
             </div>
-            <div className="w-1/2 mt-8 lg:w-1/4 md:w-1/3 md:mt-0">
+            <div className="w-1/2 mt-8 xxl:w-1/4 xl:w-1/3 xl:mt-0">
               <p className="text-white font-bold text-lg">Places</p>
 
               <ul className="flex flex-col items-start gap-2 mt-5">
@@ -44,7 +44,7 @@ function Footer() {
                 </li>
               </ul>
             </div>
-            <div className="w-1/2 mt-8 lg:w-1/4 md:w-1/3 md:mt-0">
+            <div className="w-1/2 mt-8 xxl:w-1/4 xl:w-1/3 xl:mt-0">
               <p className="text-white font-bold text-lg">About us</p>
 
               <ul className="flex flex-col items-start gap-2 mt-5">
@@ -62,24 +62,24 @@ function Footer() {
                 </li>
               </ul>
             </div>
-            <div className="mt-12 flex w-full gap-6 flex-col-reverse lg:w-1/4  lg:flex-col lg:justify-between lg:order-first lg:mt-0">
+            <div className="mt-12 flex w-full gap-6 flex-col-reverse xl:w-1/4  xl:flex-col xl:justify-between xl:order-first xl:mt-0">
               <div id="logo" className="w-[150px] md:w-[230px]">
                 <Logo />
               </div>
-              <div className="flex  gap-4 md:gap-8">
+              <div className="flex  gap-4 xl:gap-8">
                 <a href="https://facebook.com">
-                  <Fb className="h-10 w-10 transition-all duration-300 ease-linear text-[#F7F7F7] hover:text-[#a02279] md:w-auto md:h-auto" />
+                  <Fb className="h-10 w-10 transition-all duration-300 ease-linear text-[#F7F7F7] hover:text-[#a02279] lg:w-auto lg:h-auto" />
                 </a>
                 <a href="https://facebook.com">
-                  <Ig className="h-10 w-10 transition-all duration-300 ease-linear text-[#F7F7F7] hover:text-[#a02279] md:w-auto md:h-auto" />
+                  <Ig className="h-10 w-10 transition-all duration-300 ease-linear text-[#F7F7F7] hover:text-[#a02279] lg:w-auto lg:h-auto" />
                 </a>
                 <a href="https://facebook.com">
-                  <Tw className="h-10 w-10 transition-all duration-300 ease-linear text-[#F7F7F7] hover:text-[#a02279] md:w-auto md:h-auto" />
+                  <Tw className="h-10 w-10 transition-all duration-300 ease-linear text-[#F7F7F7] hover:text-[#a02279] lg:w-auto lg:h-auto" />
                 </a>
               </div>
             </div>
           </div>
-          <div className="mt-7 md:mt-14">
+          <div className="mt-7 lg:mt-14">
             <p> &copy; 2022 Metabnb</p>
           </div>
         </div>
